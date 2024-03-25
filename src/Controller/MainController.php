3a9ae81a6c4feed019e0172ba46b1ac6712e0efa
Sushiette <itomane57@gmail.com>
2024-03-25 15:45:58 +0100

    avancées notamment au niveau de "mon profil"

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -14,4 +14,5 @@ class MainController extends AbstractController
         return $this->render('main/home.html.twig');
     }
 
+
 }
---
+++ src/Controller/UserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class UserController extends AbstractController
+{
+    #[Route('/profile', name: 'app_main_profile', methods: ['GET', 'POST'])]
+    public function profile(): Response
+    {
+    return $this->render('main/profile.html.twig');
+
+    }
+}
