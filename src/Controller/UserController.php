cb21cac67a47bbb607803e9172d7a0cdbeecd0ae
Sushiette <itomane57@gmail.com>
2024-03-26 15:52:49 +0100

    changement apportés, création de compte, navbar, etc

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,23 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/profile', name: 'app_main_profile', methods: ['GET', 'POST'])]
     public function profile(): Response
     {
         $user = $this->getUser();
+        $campuses = $this->entityManager->getRepository(Campus::class)->findAll();
 
-    return $this->render('main/profile.html.twig', [
-        'user' => $user,
-
-
-    ]);
+        return $this->render('main/profile.html.twig', [
+            'user' => $user,
+            'campuses' => $campuses,
+        ]);
     }
-
 }
