7cd801aa5c27e4e7b935405b373c18c6953c463b
NathanCorroyer <nathan.corroyer8594@gmail.com>
2024-03-26 12:18:34 +0100

    Fonction inscription à une activité

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -2,12 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\ActivityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ActivityController extends AbstractController
 {
+
+    public function __construct(private readonly ValidatorInterface $validator)
+    {
+    }
+
+    #[Route('/activity/register/{id}', name : '')]
+    public function addUsersToActivity(int $id, ActivityRepository $activityRepository) : Response
+    {
+        $activity = $activityRepository->find($id);
+
+        $violations = $this->validator->validate($activity);
+
+        if (count($violations) > 0) {
+            throw new \Exception('Impossible, il y a déjà trop d\'utilisateurs inscrits');
+        }
+
+        /** @var User $user */
+        $user = $this->getUser();
+        $activity->addUser($user);
+
+        return $this->render('/activity/details/'.$id, [
+            'message' => 'Vous avez bien été inscrit à cette activité'
+        ]);
+    }
     #[Route('/activity', name: 'app_activity')]
     public function index(): Response
     {
