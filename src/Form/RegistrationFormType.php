5785b838b74358b89ee524067f8fb85f9e25e27c
Sushiette <itomane57@gmail.com>
2024-03-28 11:06:15 +0100

    commit

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -49,7 +49,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit être de {{ limit }} caractères minimum',
 
                         'max' => 4096,
                     ]),
