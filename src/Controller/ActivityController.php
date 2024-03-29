6eab85c42212efcf6cdaa59684690aa58624f174
Vraën <christophehoarau14@gmail.com>
2024-03-29 12:00:03 +0100

    Implémentation du Scheduler et des messages  + paramétrage de la vérification 1 fois par jour du statut des activités

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -6,12 +6,16 @@ use App\Entity\Activity;
 use App\Entity\State;
 use App\Entity\User;
 use App\Form\CreateActivityType;
+use App\Message\ArchiveActivityMessage;
 use App\Repository\ActivityRepository;
+
 use App\Repository\PlaceRepository;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -211,6 +215,14 @@ class ActivityController extends AbstractController
         return $this->render('activity/edit.html.twig', [
             'activityForm' => $activityForm->createView(),
         ]);
+    }
+
+    #[Route('/archive' , name : 'archive' )]
+    public function archiveById( MessageBusInterface $messageBus ) : Response {
+
+        $messageBus->dispatch( new ArchiveActivityMessage());
+
+        return $this->redirectToRoute('app_main_home');
     }
 
 }
